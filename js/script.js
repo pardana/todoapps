ca4d7f9fbfba53ceb1de951c98127647cbe9ee55
Aditria Pardana <aditria.pardana20@gmail.com>
2022-06-23 12:04:14 +0700

    update js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
 const todos = [];
 const RENDER_EVENT = "render-todo";
 
+document.addEventListener("DOMContentLoaded", function () {
+  const submitForm = document.getElementById("form");
+  submitForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    addTodo();
+  });
+});
+
 function addTodo() {
   const textTodo = document.getElementById("title").value;
   const timestamp = document.getElementById("date").value;
 
-  const generateId = generateId();
-  const todoObject = generateTodoObject(generateId, textTodo, timestamp, false);
+  const generatedID = generateId();
+  const todoObject = generateTodoObject(
+    generatedID,
+    textTodo,
+    timestamp,
+    false
+  );
   todos.push(todoObject);
 
   document.dispatchEvent(new Event(RENDER_EVENT));
@@ -24,14 +37,6 @@ function generateTodoObject(id, task, timestamp, isCompleted) {
     isCompleted,
   };
 }
-
-document.addEventListener("DOMContentLoaded", function () {
-  const submitForm = document.getElementById("form");
-  submitForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-    addTodo();
-  });
-});
 
 document.addEventListener(RENDER_EVENT, function () {
   console.log(todos);
